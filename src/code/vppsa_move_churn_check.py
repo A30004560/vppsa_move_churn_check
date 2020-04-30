d1a006f1326fe25d544d9d6f688d8fbbd9d346d9
Javad Jazaeri <60163916+A30004560@users.noreply.github.com>
2020-05-01 09:24:13 +1000

    Update vppsa_move_churn_check.py

--- src/code/vppsa_move_churn_check.py
+++ src/code/vppsa_move_churn_check.py
@@ -58,7 +58,7 @@ def check_email(user,sender_email, path_input,path_output, reciever_emails):
     t_exportfile_code_0 = time.time()
     today = datetime.today().date()   
     
-    path_output_file = path_output + "/Full VPPSA Site List V4 outputfile {datetime}.xlsx" .format(datetime=today)    
+    path_output_file = path_output + "/Full VPPSA Site List V3 outputfile {datetime}.xlsx" .format(datetime=today)    
           
     df2.to_excel(path_output_file)
     t_exportfile_code_1 = time.time()
@@ -101,9 +101,9 @@ def check_email(user,sender_email, path_input,path_output, reciever_emails):
             
             Kind regards,
             
-            Javad Jazaeri'''.format(today_date = today, category_all_num = category_all, category_2_num = category_2, category_3_num = category_3, category_4_num =category_4, category_1 = category_1, path_output_file_loc = path_output_file)
+            Javad'''.format(today_date = today, category_all_num = category_all, category_2_num = category_2, category_3_num = category_3, category_4_num =category_4, category_1 = category_1, path_output_file_loc = path_output_file)
     
-        #message.attach(path_output_file)
+        message.attach(path_output_file)
         
         sender = mailer.Mailer('aglsmtp05.agl.com.au')
         
