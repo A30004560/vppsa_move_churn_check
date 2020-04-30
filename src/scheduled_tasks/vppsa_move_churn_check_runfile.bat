71c37ea78270fe93bdc52933ea173bdac9c103ea
Javad Jazaeri <60163916+A30004560@users.noreply.github.com>
2020-05-01 09:23:00 +1000

    bat file created

--- src/scheduled_tasks/vppsa_move_churn_check_runfile.bat
+++ src/scheduled_tasks/vppsa_move_churn_check_runfile.bat
@@ -1,1 +1,6 @@
-rb
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+call pushd "\\glawi222\data$\Data\Common\New Energy\Commercial Development\Data strategy and analytics\12 vppsa-move-and-churn-check\rep1_test\"
+call activate myenv_test1
+call python -m vppsa_move_churn_check "jjazaeri@agl.com.au" "P:/New Energy/Churn Moveout Report/Input_file/Full VPPSA Site List V3.xlsx" "P:/New Energy/Churn Moveout Report/Output_file" "TKelly-Spanner@agl.com.au"
+call popd
+pause
